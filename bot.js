792c24e9e1f86c134d3bfa3f222627c10e366960
Arturo Dominguez <adominguez@nearsoft.com>
2017-02-06 16:01:30 -0700

    search states

--- bot.js
+++ bot.js
@@ -7,14 +7,40 @@ module.exports = {
 		if(!options) {
 			options = {};
 		}
-		
+
+		var states = {};
+
 		var lang = options.lang || 'es-MX';
+		var name = options.botName;
 
 		bot.onText(/\/search (.+)/, (msg, match) => {
 			var chatId = msg.chat.id;
 
+			search(match[1], msg);
+		});
+
+		var regex = new RegExp(`/search@${name}$`);
+		bot.onText(/\/search$/, initSearch);
+		bot.onText(regex, initSearch);
+
+		bot.on('message', (msg) => {
+			if(getState(msg)) {
+				deleteState(msg);
+				search(msg.text, msg);
+			}
+		});
+
+		function initSearch(msg, match) {
+			var chatId = msg.chat.id;
+			setState(true, msg);
+			bot.sendMessage(chatId, 'What do you want to search?');
+		}
+
+		function search(text, msg) {
+			var chatId = msg.chat.id;
+
 			var options = {
-				uri: `http://api.duckduckgo.com/?q=${match[1]}&format=json`,
+				uri: `http://api.duckduckgo.com/?q=${text}&format=json`,
 				json: true,
 				headers: {
 					'Accept-Language': lang
@@ -36,6 +62,22 @@ module.exports = {
 				}
 			})
 			.catch((error) => bot.sendMessage(chatId, 'Oops. There was an error. Try again later'));
-		});
+		}
+
+		function getState(msg) {
+			return states[getId(msg)];
+		}
+
+		function setState(val, msg) {
+			states[getId(msg)] = val;
+		}
+
+		function deleteState(msg) {
+			delete states[getId(msg)];
+		}
+
+		function getId(msg) {
+			return `${msg.from.id}-${msg.chat.id}`;
+		}
 	}
 };
--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 
 var TelegramBot = require('node-telegram-bot-api');
 
-var token = process.env.TOKEN || '';
+var token = process.env.TOKEN;
 var lang = process.env.LANG;
+var botName = process.env.NAME;
+
+if(!token || ! botName) {
+	throw 'TOKEN and NAME environment variables are required!';
+}
 
 var telegramBot = new TelegramBot(token, { polling: true });
 
 var searchBot = require('./bot');
 searchBot.init(telegramBot, {
-	lang: lang
+	lang: lang,
+	botName: botName
 });
 
 console.log('Search bot started');
