72fb8fe664390756bdf6064bc7dd038732261658
Arturo Dominguez <adominguez@nearsoft.com>
2017-02-10 15:47:58 -0700

    heroku stuff

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ searchBot.init(telegramBot, {
 });
 
 console.log('Search bot started');
+
+var app = require('express')();
+app.get('/', (req, res) => res.send('Running!'))
+app.listen(process.env.PORT || 3000);
