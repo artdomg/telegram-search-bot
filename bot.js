e80a9d7d0fb0280df5933cec6335812e0ad297f4
Arturo Dominguez <adominguez@nearsoft.com>
2017-02-10 19:48:33 -0700

    mercadolibre search

--- bot.js
+++ bot.js
@@ -10,48 +10,101 @@ module.exports = function(bot, options) {
 
 	var name = options.botName;
 
+	var commands = {
+		'MercadoLibre': function(text, chatId) {
+			return search.mercadolibreSearch(text)
+			.then((data) => {
+				if(data.results.length === 0) {
+					return bot.sendMessage(chatId, 'Could not find anything');
+				}
+
+				var article = data.results[0];
+
+				bot.sendMessage(chatId, `${article.title} - ${article.price} ${article.currency_id} - ${article.permalink}`);
+
+				return data;
+			});
+		},
+		'DuckDuckGo': function(text, chatId) {
+			return search.generalSearch(text, { lang: options.lang })
+			.then((data) => {
+				if(!data.result && !data.image) {
+					return bot.sendMessage(chatId, 'Could not find anything.');
+				}
+
+				if(data.result) bot.sendMessage(chatId, data.result);
+				if(data.image) bot.sendPhoto(chatId, data.image);
+
+				return data;
+			});
+		}
+	};
+
 	return {
 		searchInline: (msg, match) => {
 			var chatId = msg.chat.id;
+
+			stateManager.setState({
+				status: 2,
+				command: 'DuckDuckGo'
+			}, msg);
 
 			searchText(match[1], msg);
 		},
 		initSearch: (msg, match) => {
 			var chatId = msg.chat.id;
-			stateManager.setState(true, msg);
-			bot.sendMessage(chatId, 'What do you want to search?', {
+			stateManager.setState({
+				status: 1
+			}, msg);
+			bot.sendMessage(chatId, 'Where do you want to search?', {
 				reply_to_message_id: msg.message_id,
 				reply_markup: {
+					keyboard: [
+						[
+							{ text: 'MercadoLibre' },
+							{ text: 'DuckDuckGo' }
+						]
+					],
 					force_reply: true,
 					selective: true
 				}
 			});
 		},
 		search: (msg) => {
-			if(stateManager.getState(msg)) {
-				stateManager.deleteState(msg);
-				searchText(msg.text, msg);
+			var chatId = msg.chat.id;
+			var state = stateManager.getState(msg);
+
+			if(state) {
+				if(state.status === 1) {
+					if(commands[msg.text]) {
+						state.status = 2;
+						state.command = msg.text;
+						bot.sendMessage(chatId, 'What do you want to search?', {
+							reply_to_message_id: msg.message_id,
+							reply_markup: {
+								force_reply: true,
+								selective: true
+							}
+						});
+					} else {
+						stateManager.deleteState(msg);
+					}
+				} else if(state.status === 2) {
+					searchText(msg.text, msg);
+				}
 			}
 		}
 	};
 
 	function searchText(text, msg) {
 		var chatId = msg.chat.id;
+		var state = stateManager.getState(msg);
 
-		search(text, { lang: options.lang })
-		.then((data) => {
-			if(!data.result && !data.image) {
-				return bot.sendMessage(chatId, 'Could not find anything.');
-			}
+		if(state) {
+			stateManager.deleteState(msg);
 
-			if(data.result) {
-				bot.sendMessage(chatId, data.result);
-			}
-
-			if(data.image) {
-				bot.sendPhoto(chatId, data.image);
-			}
-		})
-		.catch(() => bot.sendMessage(chatId, 'Oops. There was an error. Try again later'));
+			commands[state.command](text, chatId)
+			.catch(() => bot.sendMessage(chatId, 'Oops. There was an error. Try again later'));
+		}
 	}
 };
--- search.js
+++ search.js
@@ -2,23 +2,33 @@
 
 var rp = require('request-promise');
 
-module.exports = function (text, options) {
-	if(!options) options = {};
-	if(!options.lang) options.lang = 'es-MX';
+module.exports = {
+	generalSearch: function (text, options) {
+		if(!options) options = {};
+		if(!options.lang) options.lang = 'es-MX';
 
-	var reqOptions = {
-		uri: `http://api.duckduckgo.com/?q=${text}&format=json`,
-		json: true,
-		headers: {
-			'Accept-Language': options.lang
-		}
-	};
-
-	return rp(reqOptions)
-	.then((data) => {
-		return {
-			result: data.AbstractText,
-			image: data.Image
+		var reqOptions = {
+			uri: `http://api.duckduckgo.com/?q=${text}&format=json`,
+			json: true,
+			headers: {
+				'Accept-Language': options.lang
+			}
 		};
-	});
-}
+
+		return rp(reqOptions)
+		.then((data) => {
+			return {
+				result: data.AbstractText,
+				image: data.Image
+			};
+		});
+	},
+	mercadolibreSearch: function(text) {
+		var reqOptions = {
+			uri: `https://api.mercadolibre.com/sites/MLM/search?q=${text}`,
+			json: true
+		};
+
+		return rp(reqOptions);
+	}
+};
