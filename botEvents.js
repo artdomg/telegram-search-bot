041579cb79cb748aa70a8f097f3dd18e1868d725
Arturo Dominguez <adominguez@nearsoft.com>
2017-02-10 20:43:24 -0700

    removed instant search

--- botEvents.js
+++ botEvents.js
@@ -6,12 +6,12 @@ module.exports = {
 	init: (bot, options) => {
 		var botController = botModule(bot, options);
 
-		bot.onText(/\/search (.+)/, botController.searchInline);
+		//bot.onText(/\/search (.+)/, botController.searchInline);
 
-		bot.onText(/\/search$/, botController.initSearch);
+		bot.onText(/\/search/, botController.initSearch);
 
-		var regex = new RegExp(`/search@${options.name}$`);
-		bot.onText(regex, botController.initSearch);
+		//var regex = new RegExp(`/search@${options.name}$`);
+		//bot.onText(regex, botController.initSearch);
 
 		bot.on('message', botController.search);
 	}
